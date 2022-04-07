30e7f66fe5186b36b71f7bcd50db9c22e2be9440
verdulife <verdu@live.com>
2022-04-07 15:20:49 +0200

    new fotos in gallery

--- src/routes/form.ts
+++ src/routes/form.ts
@@ -1,40 +1,43 @@
 import { SMTPClient } from 'emailjs';
 
+//const emailTo = 'sitges@seastone.es';
+const emailTo = 'verdukactus@gmail.com';
+
 export async function post({ request }) {
-  const { name, email, phone, message } = await request.json();
+	const { name, email, phone, message } = await request.json();
 
-  const client = new SMTPClient({
-    user: import.meta.env.VITE_EMAIL_USER,
-    password: import.meta.env.VITE_EMAIL_PASS,
-    host: 'smtp.ionos.es',
-    ssl: true,
-  });
+	const client = new SMTPClient({
+		user: import.meta.env.VITE_EMAIL_USER as string,
+		password: import.meta.env.VITE_EMAIL_PASS as string,
+		host: 'smtp.ionos.es',
+		ssl: true
+	});
 
-  client.send(
-    {
-      from: `Seastone Homes Sitges Web <${import.meta.env.VITE_EMAIL_USER}>`,
-      to: `Seastone Homes Sitges Web <${import.meta.env.VITE_EMAIL_USER}>`,
-      subject: name,
-      text: `
+	client.send(
+		{
+			from: `Seastone Homes Sitges Web <${import.meta.env.VITE_EMAIL_USER}>`,
+			to: `Seastone Homes Sitges <${emailTo}>`,
+			subject: name,
+			text: `
 Nombre: ${name}
 Email: ${email}
 Teléfono: ${phone}
 \n
   ${message}
-`,
-    },
-    (err, message) => {
-      return {
-        body: {
-          status: 'error',
-        }
-      };
-    }
-  );
+`
+		},
+		() => {
+			return {
+				body: {
+					status: 'error'
+				}
+			};
+		}
+	);
 
-  return {
-    body: {
-      status: 'ok',
-    }
-  };
+	return {
+		body: {
+			status: 'ok'
+		}
+	};
 }
